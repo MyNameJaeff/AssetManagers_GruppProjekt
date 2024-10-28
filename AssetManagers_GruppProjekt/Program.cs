6adc540c147ebf09dfb78ba9357873b2ed6d939e
MyNameJaeff <fredrik.andersson@chasacademy.se>
2024-10-28 15:27:40 +0100

    Init console app and repo

---
+++ AssetManagers_GruppProjekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssetManagers_GruppProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
